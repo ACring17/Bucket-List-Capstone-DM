12e79549d0adea5d9b06f89431abed6b3f6d3ce8
Andrew Cring <amcring1@gmail.com>
2022-05-24 17:09:20 -0700

    Making some changes to UI and backend to help handle the buttons on the page.

--- public/index.js
+++ public/index.js
@@ -1,38 +1,10 @@
 const addBucket = document.getElementById("#addBucket")
 
-function showUnexpectedErrorAlert() {
-    alert('Sorry, an unexpected error occurred. Please try again later!');
-}
+// Code to display lists
 
-async function getCharacters() {
-    try {
-        const res = await axios.get('/api/characters');
+// Code to hide buttons until list items are added
 
-        renderCharacters(res.data);
-    } catch (error) {
-        showUnexpectedErrorAlert();
-    }
-}
-
-function renderBucketList() {
-    const bucketListContainer = document.getElementById('#bucket-list-container');
-
-    for (const items of items) {
-        const li = document.createElement('li');
-        li.addEventListener('dblclick', () => {
-            const nameInput = document.getElementsByName('name')[0];
-
-
-            nameInput.value = character.name;
-            levelInput.value = character.level;
-            methodInput.value = 'patch';
-            idInput.value = character.id;
-            button.innerText = 'Update Character';
-        });
-
-        bucketListContainer.appendChild(li);
-    }
-}
+// code to move list items from bucket list to achieved list
 
 // document.getElementById("complimentButton").onclick = function() {
 //     axios.get("http://localhost:4000/api/compliment/")
