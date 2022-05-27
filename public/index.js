b7e9327d563fa60247755e12c9315aa314488239
Andrew Cring <amcring1@gmail.com>
2022-05-26 18:21:49 -0700

    Been working on the end points for the GET and POST methods

--- public/index.js
+++ public/index.js
@@ -1,6 +1,37 @@
-const addBucket = document.getElementById("#addBucket")
+const { createElement } = require("react/cjs/react.production.min")
 
-// Code to display lists
+// Post methods
+document.getElementById('submitBtn').onclick = function(event) {
+    event.preventDefault()
+    const addBucket = document.getElementById("addBucket")
+    console.log(addBucket.value)
+    const goals = { DataBaseName: addBucket.value }
+    axios.post('http://localhost:4040/api/bucketList', goals).then(res => {
+            console.log(res.data)
+        }) //Will need to change DataBaseName to whatever I need to connnect to heroku. Not sure if I need the console.log
+}
+
+
+// Code to display lists-Get requests
+document.getElementById("bucket-list").onclick = function() {
+    axios.get("http://localhost:4040/api/bucketList/")
+        .then(function(response) {
+            const data = response.data;
+            const listItems = document.createElement("ul");
+            data.appendChild(listItems);
+            document.body.append("bucket-list");
+        });
+};
+
+document.getElementById("achievement-list").onclick = function() {
+    axios.get("http://localhost:4040/api/achievementList/")
+        .then(function(response) {
+            const data = response.data;
+            const listItems = document.createElement("ul");
+            data.appendChild(listItems);
+            document.body.append("achievment-list");
+        });
+};
 
 // Code to hide buttons until list items are added
 
